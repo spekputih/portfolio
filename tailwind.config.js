fcb5e8ce81ae99ac813ab67c53dbca34ceec67e2
Afiq Syazwan <fafiqsyaz@gmail.com>
2022-02-02 21:23:15 +0800

    add navbar

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./pages/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        highlight: "Comforter",
+        primary: 'IBM Plex Sans Thai Looped'
+      }
+    },
+  },
+  plugins: [],
+}
