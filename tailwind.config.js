bbf82af34463e0d44b1e707da6e8141b818f1ff9
Afiq Syazwan <fafiqsyaz@gmail.com>
2022-02-03 01:51:57 +0800

    change asset

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,28 @@ module.exports = {
       fontFamily: {
         highlight: "Comforter",
         primary: 'IBM Plex Sans Thai Looped'
+      },
+      height: {
+        screen10: '10vh',
+        screen20: '20vh',
+        screen30: '30vh',
+        screen40: '40vh',
+        screen50: '50vh',
+        screen60: '60vh' ,
+        screen70: '70vh',
+        screen80: '80vh',
+        screen90: '90vh' 
+      },
+      width: {
+        screen10: '10vw',
+        screen20: '20vw',
+        screen30: '30vw',
+        screen40: '40vw',
+        screen50: '50vw',
+        screen60: '60vw' ,
+        screen70: '70vw',
+        screen80: '80vw',
+        screen90: '90vw'  
       }
     },
   },
